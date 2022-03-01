3c5061855cc2fc89d86bd4538d36e1d908b3c2c8
Nicolas Eloy <colasrsca@gmail.com>
2022-03-01 17:33:38 +0100

    corr err typo crypto

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Routes , Route, Link } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
 
-import { Navbar } from './components';
+import { Navbar, Homepage, Exchanges, Cryptocurrencies, CryptoDetails, News } from './components';
 import './App.css';
 
 const App = () => {
@@ -12,7 +12,17 @@ const App = () => {
         <Navbar />
       </div>
       <div className='main'>
-
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route exact path='/' element={<Homepage/>} />
+              <Route exact path='/exchanges' element={<Exchanges />} />
+              <Route exact path='/Cryptocurrencies' element={<Cryptocurrencies/>} />
+              <Route exact path='/crypto/:coinId' element={<CryptoDetails/>} />
+              <Route exact path='/news' element={<News/>} />
+            </Routes>
+          </div>
+        </Layout>
       </div>
       <div className='footer'>
 
