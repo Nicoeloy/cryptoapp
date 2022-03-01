d3f5caa8bb26feb6e97f90a76aedf307f073b154
Nicolas Eloy <colasrsca@gmail.com>
2022-03-01 18:11:10 +0100

    add redux store

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,17 @@
+// var options = {
+//   method: 'GET',
+//   url: 'https://coinranking1.p.rapidapi.com/coins',
+//   params: {
+//     referenceCurrencyUuid: 'yhjMzLPhuIDl',
+//     timePeriod: '24h',
+//     tiers: '1',
+//     orderBy: 'marketCap',
+//     orderDirection: 'desc',
+//     limit: '50',
+//     offset: '0'
+//   },
+//   headers: {
+//     'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+//     'x-rapidapi-key': 'ff42fde3f6msh880118e740f716dp1a1c8cjsnce572d8ff997'
+//   }
+// };
